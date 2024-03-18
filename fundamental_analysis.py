6849b99494560c1a66108af56e1f5724043ba4b2
Sahil Shah <sahil@MacBook-Pro.localdomain>
2024-03-18 14:13:26 -0400

    creating mapping between local/yfinance fields & adding exception handle for missing data

--- fundamental_analysis.py
+++ fundamental_analysis.py
@@ -1,5 +1,6 @@
 import yfinance
 import pandas
+import numpy
 import warnings
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -9,9 +10,9 @@ def style_fundamentals_dataframe(styler):
     # Column formatting
     styler.format({'Forward Earnings Per Share (EPS)': '${:.2f}', 'Forward Price to Earnings Ratio (P/E)': '{:.2f}',
                    'Price/Earnings-to-Growth Ratio (PEG)': '{:.2f}',
-                   'FCFY': '{:.2f}%', 'Price to Book Ratio (P/B)': '{:.2f}', 'Return on Equity (ROE)': '{:.2f}',
+                   'Free Cash Flow Yield (FCFY)': '{:.2f}', 'Price to Book Ratio (P/B)': '{:.2f}', 'Return on Equity (ROE)': '{:.2f}',
                    '12-Month Trailing Price-to-Sales Ratio (P/S)': '{:.2f}',
-                   'Dividend Payout Ratio (DPR)': '{:.2f}%', 'Dividend Yield (DY)': '{:.2f}%',
+                   'Dividend Payout Ratio (DPR)': '{:.2f}', 'Dividend Yield (DY)': '{:.2f}%',
                    'Current Ratio (CR)': '{:.2f}', 'Beta': '{:.2f}', '52-Week Low': '${:.2f}',
                    'Price': '${:.2f}', '52-Week High': '${:.2f}'})
 
@@ -22,13 +23,12 @@ def style_fundamentals_dataframe(styler):
     styler.background_gradient(subset=['Forward Earnings Per Share (EPS)'], cmap='Greens')
     styler.background_gradient(subset=['Forward Price to Earnings Ratio (P/E)'], cmap='Greens')
     styler.background_gradient(subset=['Price/Earnings-to-Growth Ratio (PEG)'], cmap='Greens')
-    # styler.background_gradient(subset=['FCFY'], cmap='Greens')
     styler.background_gradient(subset=['Price to Book Ratio (P/B)'], cmap='Greens')
     styler.background_gradient(subset=['Return on Equity (ROE)'], cmap='Greens')
     styler.background_gradient(subset=['12-Month Trailing Price-to-Sales Ratio (P/S)'], cmap='Greens')
     styler.background_gradient(subset=['Dividend Payout Ratio (DPR)'], cmap='Greens')
     styler.background_gradient(subset=['Dividend Yield (DY)'], cmap='Greens')
-    # styler.background_gradient(subset=['Current Ratio (CR)'], cmap='Greens')
+    styler.background_gradient(subset=['Current Ratio (CR)'], cmap='Greens')
 
     # Remove index column
     styler.hide(axis='index')
@@ -45,47 +45,59 @@ def fetch_stock_info(stocks):
         'Forward Earnings Per Share (EPS)': [],
         'Forward Price to Earnings Ratio (P/E)': [],
         'Price/Earnings-to-Growth Ratio (PEG)': [],
-        # 'Free Cash Flow Yield (FCFY)': []],
         'Price to Book Ratio (P/B)': [],
         'Return on Equity (ROE)': [],
         '12-Month Trailing Price-to-Sales Ratio (P/S)': [],
         'Dividend Payout Ratio (DPR)': [],
         'Dividend Yield (DY)': [],
-        # 'Current Ratio (CR)': []],
+        'Current Ratio (CR)': [],
+        'Free Cash Flow Yield (FCFY)': [],
         'Beta': [],
         'Price': [],
         '52-Week Low': [],
         '52-Week High': []
     }
 
+    key_yfinancekey_map = {
+        'Symbol': 'symbol',
+        'Name': 'shortName',
+        'Industry': 'industry',
+        'Forward Earnings Per Share (EPS)': 'forwardEps',
+        'Forward Price to Earnings Ratio (P/E)': 'forwardPE',
+        'Price/Earnings-to-Growth Ratio (PEG)': 'pegRatio',
+        'Price to Book Ratio (P/B)': 'priceToBook',
+        'Return on Equity (ROE)': 'returnOnEquity',
+        '12-Month Trailing Price-to-Sales Ratio (P/S)': 'priceToSalesTrailing12Months',
+        'Dividend Payout Ratio (DPR)': 'payoutRatio',
+        'Dividend Yield (DY)': 'dividendYield',
+        'Current Ratio (CR)': 'currentRatio',
+        'Free Cash Flow Yield (FCFY)': 'freeCashflow',
+        'Beta': 'beta',
+        'Price': 'currentPrice',
+        '52-Week Low': 'fiftyTwoWeekLow',
+        '52-Week High': 'fiftyTwoWeekHigh'
+    }
+
+    key_list = stock_data.keys()
+
+
     for ticker in stocks:
         # GET Yahoo Finance TICKER INFO
         yfTicker = yfinance.Ticker(ticker)
         stock_info = yfTicker.info
 
-        stock_data['Symbol'].append(stock_info['symbol'])
-        stock_data['Name'].append(stock_info['shortName'])
-        stock_data['Industry'].append(stock_info['industry'])
-        stock_data['Forward Earnings Per Share (EPS)'].append(stock_info['forwardEps'])
-        stock_data['Forward Price to Earnings Ratio (P/E)'].append(stock_info['forwardPE'])
-        stock_data['Price/Earnings-to-Growth Ratio (PEG)'].append(stock_info['pegRatio'])
-        # stock_data['Free Cash Flow Yield (FCFY)'].append(stock_info['freeCashflow'])
-        stock_data['Price to Book Ratio (P/B)'].append(stock_info['priceToBook'])
-        stock_data['Return on Equity (ROE)'].append(stock_info['returnOnEquity'])
-        stock_data['12-Month Trailing Price-to-Sales Ratio (P/S)'].append(stock_info['priceToSalesTrailing12Months'])
-        stock_data['Dividend Payout Ratio (DPR)'].append(stock_info['payoutRatio'])
-        stock_data['Dividend Yield (DY)'].append(stock_info['dividendYield'])
-        # stock_data['Current Ratio (CR)'].append(stock_info['currentRatio'])
-        stock_data['Beta'].append(stock_info['beta'])
-        stock_data['Price'].append(stock_info['currentPrice'])
-        stock_data['52-Week Low'].append(stock_info['fiftyTwoWeekLow'])
-        stock_data['52-Week High'].append(stock_info['fiftyTwoWeekHigh'])
+
+        for stock_data_key in key_list:
+            try:
+                stock_data[stock_data_key].append(stock_info[key_yfinancekey_map[stock_data_key]])
+            except KeyError:
+                stock_data[stock_data_key].append(numpy.nan)
 
     # Return a DF using the stock_data dictionary
     return pandas.DataFrame(stock_data)
 
 
-stocks = ['AAPL', 'GS', 'IBM', 'INTC', 'JNJ', 'JPM', 'MS', 'TRV']
+stocks = ['AAPL', 'GS', 'IBM', 'INTC', 'JNJ', 'JPM', 'MS', 'TRV', 'GOOG']
 stock_info_dataframe = fetch_stock_info(stocks)
 # print(stock_info_dataframe)
 
@@ -97,6 +109,6 @@ stock_info_dataframe.style.pipe(style_fundamentals_dataframe).set_caption('Funda
     }, {
         'selector': 'caption',
         'props': [('text-align', 'center'),
-                  ('font-size', '10pt'),
+                  ('font-size', '14pt'),
                   ('font-weight', 'bold')]}
     ])
